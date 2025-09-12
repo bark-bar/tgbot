83fd73a9f4664aa1729f55a21a5d6e790dc83ba6
Nick Nazarov <luckfun37@mail.ru>
2025-09-12 21:14:54 +0300

    add main code

---
+++ main.py
@@ -0,0 +1,19 @@
+import aiogram
+import asyncio
+
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+
+bot = Bot(token='Something went wrong...')
+dp = Dispatcher()
+
+@dp.message(Command('start'))
+async def stast(message: types.Message):
+    return message.answer("Starting...")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
