0b5b165813d48f0ab12aff3f651e5d8f86bb2042
Your Name <you@example.com>
2025-09-24 21:24:14 +0300

    latest updates

--- databases/methods.py
+++ databases/methods.py
@@ -30,7 +30,6 @@ class DatabaseHolder():
             await conn.execute('INSERT INTO spends(amount,user_id,currency,create_date,description) VALUES($1,$2,$3,$4,$5)',amount,user_id,currency,create_date,description)
             await tr.commit()
         except Exception as e:
-            print(f'failed to connect:{e}')
             await tr.rollback()
         await conn.close()
     async def delete_expense(self,user_id,expense_number):
@@ -41,11 +40,21 @@ class DatabaseHolder():
             await conn.execute('DELETE FROM spends as s  USING numberedspends as n  WHERE s.user_id = $1 AND n.user_id = $1 AND n.number_of_expense = $2',user_id,expense_number)
             await tr.commit()
         except Exception as e:
-            print(f'failed:{e}')
             await tr.rollback()
         await conn.close()
     async def get_user_expenses(self,user_id):
         conn = await asyncpg.connect(user='postgres',password='31591',host='localhost',database='usersandother')
-        data = await conn.fetch('SELECT description,create_date,number_of_expense FROM numberedspends where user_id = $1',user_id)
+        data = await conn.fetch('SELECT amount,description,create_date,number_of_expense FROM numberedspends where user_id = $1',user_id)
         await conn.close()
         return [tuple(record) for record in data]
+    async def delete_all_expenses(self,user_id):
+        conn = await asyncpg.connect(user='postgres',password='31591',host='localhost',database='usersandother')
+        tr = conn.transaction()
+        try:
+            await tr.start()
+            await conn.execute('DELETE FROM spends   WHERE user_id = $1 ',user_id)
+            await tr.commit()
+        except Exception as e:
+            print(f'failed:{e}')
+            await tr.rollback()
+        await conn.close()
